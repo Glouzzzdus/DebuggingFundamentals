9c778ef074619426937d2703eccccd1a6c7b3adf
Andrii_Hluzdovskyi <Andrii_Hluzdovskyi@EPUALVIW05BD>
2023-10-13 17:33:10 +0200

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product otherProduct)
+            {
+                return Name == otherProduct.Name && Price == otherProduct.Price;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name, Price).GetHashCode();
+        }
+    }    
+}
